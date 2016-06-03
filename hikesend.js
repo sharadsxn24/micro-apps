4e687670b7a44b8b9dc91d8d9421d49e03c3a7fb
Sharad Saxena <sharad@hike.in>
2016-06-03 13:42:18 +0530

    toi

--- hikesend.js
+++ hikesend.js
@@ -1,13 +1,30 @@
 (function(){
 	var hikeElem = document.querySelector('.hikeshare');
 		
+		// var now = new Date().valueOf();
+		// setTimeout(function () {
+		//     if (new Date().valueOf() - now > 100) return;
+		//     window.location = "https://itunes.apple.com/appdir";
+		// }, 25);
+		// window.location = "appname://";
+
 	function hikeShare(){
 		var title = document.title;
 		var href = location.href;
+		var now = new Date().valueOf();
 		var url = "hike://send/?text=" + encodeURIComponent(document.title) + " - " + encodeURIComponent(href);
-		openWindow(url);
+
+		setTimeout(function () {
+		    if (new Date().valueOf() - now > 100) return;
+		    // window.location = "https://play.google.com/store/apps/details?id=com.bsb.hike";
+		    window.location = "http://get.hike.in/";
+		}, 50);
+		
+		window.location = url;
+
+		//openWindow(url);
 		console.log(title);
-	console.log(url);
+		console.log(url);
 	}
 
 	function openWindow(url){
